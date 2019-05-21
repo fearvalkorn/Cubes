95b63cc5893b769827e45640d1d5b2a2ed410712
react <fearvalkorn@gmail.com>
2019-05-21 20:06:36 +0200

    loops

---
+++ CubesJS/public_html/script.js
@@ -0,0 +1,32 @@
+//var a = 10;
+//
+//var b = a;
+//
+//b = 20;
+//
+//console.log(a,b);
+
+//var a = {
+//    name: "Peter",
+//    lastName: "Dinklage"
+//};
+//
+//var b = a;
+//
+//b.name = "Tyrion";
+//
+//console.log(a, b);
+
+//var a = "Some text here";
+
+//var a = 3;
+//console.log(a === "number");
+
+
+var i = 0;
+
+while (i < 10) {
+    i++;
+}
+
+console.log(i);
